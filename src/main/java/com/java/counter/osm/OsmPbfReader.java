9af5ebdfaef190109fd2cb5c797f9df70c51b18e
Lakshminarayanan <lakxraju@gmail.com>
2019-06-30 18:10:53 +0200

    test

--- src/main/java/com/java/counter/osm/OsmPbfReader.java
+++ src/main/java/com/java/counter/osm/OsmPbfReader.java
@@ -12,124 +12,116 @@ import org.openstreetmap.osmosis.core.domain.v0_6.Relation;
 import org.openstreetmap.osmosis.core.domain.v0_6.Way;
 import org.openstreetmap.osmosis.core.task.v0_6.Sink;
 import org.openstreetmap.osmosis.pbf2.v0_6.PbfReader;
- 
+
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
-import org.openstreetmap.osmosis.core.domain.v0_6.Tag;
- 
+
 public class OsmPbfReader {
- 
+
 	static Map<Long, Node> refToNodeMap = new HashMap<>();
-	
+
 	public static Node getNodeForId(Long id) {
 		return refToNodeMap.get(id);
 	}
-	
-    public static void main(String[] args) {
-        File osmFile = new File(".\\src\\resources\\berlin-latest.osm.pbf");
-        PbfReader reader = new PbfReader(osmFile, 1);
- 
-        AtomicInteger numberOfNodes = new AtomicInteger();
-        AtomicInteger numberOfWays = new AtomicInteger();
-        AtomicInteger numberOfRelations = new AtomicInteger();
-        AtomicInteger numberOfRegionalTrains = new AtomicInteger();
-        
-        Sink nodeParseSink = new Sink() {
-			
+
+	public static void main(String[] args) {
+		File osmFile = new File(".\\src\\resources\\berlin-latest.osm.pbf");
+		PbfReader reader = new PbfReader(osmFile, 1);
+
+		AtomicInteger numberOfNodes = new AtomicInteger();
+		AtomicInteger numberOfWays = new AtomicInteger();
+		AtomicInteger numberOfRelations = new AtomicInteger();
+		AtomicInteger numberOfRegionalTrains = new AtomicInteger();
+
+		Sink nodeParseSink = new Sink() {
+
 			@Override
 			public void close() {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void complete() {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void initialize(Map<String, Object> metaData) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void process(EntityContainer entityContainer) {
 				Entity entity = entityContainer.getEntity();
 				if (entity instanceof Node) {
 					Node node = (Node) entity;
-                    refToNodeMap.put(node.getId(), node);
-                }
+					refToNodeMap.put(node.getId(), node);
+				}
 			}
 		};
- 
-        Sink sinkImplementation = new Sink() {
- 
-            public void process(EntityContainer entityContainer) {
- 
-                Entity entity = entityContainer.getEntity();
-                if (entity instanceof Node) {
-                    numberOfNodes.incrementAndGet();
-                } else if (entity instanceof Way) {
-                    numberOfWays.incrementAndGet();
-                } else if (entity instanceof Relation) {
-                    numberOfRelations.incrementAndGet();
-               
-                
-                EntityWrapper wrapper = new EntityWrapper(entity);
-                if(wrapper.isRegionalTrain()){
-                    numberOfRegionalTrains.incrementAndGet();
-                    System.out.println(wrapper.getJsonString());
-                    
-                    
-                    
-                    /*
-                    System.out.println(entity.getType() + "--" );
-                    for(Tag tag:entity.getTags()){
-                        System.out.println(tag.getKey() + " --> " + tag.getValue());
-                    }
-                    for(Map.Entry entry : entity.getMetaTags().entrySet()){
-                        System.out.println(entry.getKey() + "-->" + entry.getValue());
-                    }
-                    Node node = (Node) entity;
-                    */
-                    //System.out.println(entity.getTags());
-                	}
-                }
-            }
- 
-            public void initialize(Map<String, Object> arg0) {
-            }
- 
-            public void complete() {
-            }
- 
-            public void release() {
-            }
 
-            @Override
-            public void close() {
-            }
- 
-        };
-        
-        reader.setSink(nodeParseSink);
-        reader.run();
-        
-        reader.setSink(sinkImplementation);
-        reader.run();
-        
-        System.out.println("Total Regional Trains Found : " + numberOfRegionalTrains);
- 
-        /*
-        System.out.println(numberOfNodes.get() + " Nodes are found.");
-        System.out.println(numberOfWays.get() + " Ways are found.");
-        System.out.println(numberOfRelations.get() + " Relations are found.");
-        System.out.println(numberOfTrainRelations + " train routes are found.");
-        */
-    }
- 
+		Sink sinkImplementation = new Sink() {
+
+			public void process(EntityContainer entityContainer) {
+
+				Entity entity = entityContainer.getEntity();
+				if (entity instanceof Node) {
+					numberOfNodes.incrementAndGet();
+				} else if (entity instanceof Way) {
+					numberOfWays.incrementAndGet();
+				} else if (entity instanceof Relation) {
+					numberOfRelations.incrementAndGet();
+
+					RelationWrapper wrapper = new RelationWrapper((Relation) entity);
+					if (wrapper.isRegionalTrain()) {
+						numberOfRegionalTrains.incrementAndGet();
+						System.out.println(wrapper.getJsonString());
+
+						/*
+						 * System.out.println(entity.getType() + "--" ); for(Tag tag:entity.getTags()){
+						 * System.out.println(tag.getKey() + " --> " + tag.getValue()); } for(Map.Entry
+						 * entry : entity.getMetaTags().entrySet()){ System.out.println(entry.getKey() +
+						 * "-->" + entry.getValue()); } Node node = (Node) entity;
+						 */
+						// System.out.println(entity.getTags());
+					}
+				}
+			}
+
+			public void initialize(Map<String, Object> arg0) {
+			}
+
+			public void complete() {
+			}
+
+			public void release() {
+			}
+
+			@Override
+			public void close() {
+			}
+
+		};
+
+		reader.setSink(nodeParseSink);
+		reader.run();
+
+		reader.setSink(sinkImplementation);
+		reader.run();
+
+		System.out.println("Total Regional Trains Found : " + numberOfRegionalTrains);
+
+		/*
+		 * System.out.println(numberOfNodes.get() + " Nodes are found.");
+		 * System.out.println(numberOfWays.get() + " Ways are found.");
+		 * System.out.println(numberOfRelations.get() + " Relations are found.");
+		 * System.out.println(numberOfTrainRelations + " train routes are found.");
+		 */
+	}
+
 }
